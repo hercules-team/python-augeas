a6c04dcc428de82fa07c1d6ebda79c8b67c25111
Harald Hoyer <harald@redhat.com>
2008-04-23 23:32:02 +0200

    begin of UnitTests in test_augeas.py

--- test_augeas.py
+++ test_augeas.py
@@ -1,9 +1,31 @@
+import unittest
+import sys
 import augeas
 
-a = augeas.augeas()
 
-for i in a.match("/files/etc/hosts/*"):
-	for attr in a.match(i+"/*"):
-                print attr, a.get(attr)
+class TestAugeas(unittest.TestCase):
+    def testGet(self):
+        "test aug_get"
+        a = augeas.augeas()
+        self.failUnless(a.get("/wrong/path") == None)
 
-print a.get("/wrong/path")
+    def testMatch(self):
+        "test aug_match"
+        a = augeas.augeas()
+        matches = a.match("/files/etc/hosts/*")
+        self.failUnless(matches)
+        for i in matches:
+	    for attr in a.match(i+"/*"):
+                self.failUnless(a.get(attr) != None)
+
+def suite():
+    suite = unittest.TestSuite()
+    suite = unittest.makeSuite(TestAugeas, 'test')
+    return suite
+
+if __name__ == "__main__":
+    testRunner = unittest.TextTestRunner(verbosity=2)
+    result = testRunner.run(suite())
+    sys.exit(not result.wasSuccessful())
+
+__author__ = "Harald Hoyer <harald@redhat.com>"
