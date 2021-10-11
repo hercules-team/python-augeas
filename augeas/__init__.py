hercules-team/python-augeas

commit bd62d0960ca1771f444ec12cf549706ca327bbac

Close augeas handle correctly. Fixes #30.

augeas/__init__.py
@@ -105,7 +105,7 @@ class Augeas(object):
         loadpath = enc(loadpath) if loadpath else ffi.NULL
 
         # Create the Augeas object
-        self.__handle = ffi.gc(lib.aug_init(root, loadpath, flags), lib.aug_close)
+        self.__handle = ffi.gc(lib.aug_init(root, loadpath, flags), lambda x: self.close)
         if not self.__handle:
             raise RuntimeError("Unable to create Augeas object!")
 
@@ -568,7 +568,7 @@ class Augeas(object):
         for any more operations."""
 
         # If we are already closed, return
-        if not self.__handle:
+        if not self.__handle or self.__handle == ffi.NULL:
             return
 
         # Call the function

test/test_augeas.py
@@ -98,7 +98,7 @@ class TestAugeas(unittest.TestCase):
             for attr in a.match(i+"/*"):
                 self.failUnless(a.get(attr) != None)
         del a
-        
+
     def test06Defnode(self):
         "test defnode"
         a = augeas.Augeas(root=MYROOT)
@@ -254,6 +254,10 @@ class TestAugeas(unittest.TestCase):
         matches = a.match(copy_path)
         self.failUnless(matches)
         self.assertEqual(a.get(copy_path), a.get(orig_path))
+
+    def testClose(self):
+        a = augeas.Augeas(root=MYROOT)
+        a.close()
 
 
 def getsuite():

commit aafba109af7665d372446af0543c69dac086ddbe

correctly close Augeas instance when ffi handle is garbage collected
Previously the lambda would only return (but not call) a reference to
`self.close`. This bug was introduced in commit bd62d096.

augeas/__init__.py
@@ -188,7 +188,7 @@ class Augeas(object):
 
         # Create the Augeas object
         self.__handle = ffi.gc(lib.aug_init(root, loadpath, flags),
-                               lambda x: self.close)
+                               lambda x: self.close())
         if not self.__handle:
             raise RuntimeError("Unable to create Augeas object!")
 